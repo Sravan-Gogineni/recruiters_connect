4a143494ce41c86645a87929abae33fa32f4871b
Sravan Chowdary Gogineni <65498582+Sravan-Gogineni@users.noreply.github.com>
2025-01-07 00:39:08 -0500

    Delete proxy_request.py

--- proxy_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Make a request through the proxy
-url = "http://www.google.com"
-response = requests.get(url)
-
-# Print the response
-print(response.status_code)
-print(response.json())
